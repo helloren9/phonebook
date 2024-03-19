cb733cc8819d63ffc73fa221ccca93ccdede89be
Loren Anunciacion <loren.anunciacion@gmail.com>
2024-03-18 23:48:48 -0700

    Add files via upload

--- app/authentication/routes.py
+++ app/authentication/routes.py
@@ -1,6 +1,6 @@
 from forms import UserLoginForm
 from models import User, db, check_password_hash
-from flask import Blueprint, render_template, request, reditect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 
 from flask_login import login_user, logout_user, LoginManager, current_user, login_required
 
@@ -37,4 +37,18 @@ def signin():
             password = form.password.data
             print(email, password)
 
-            logged_user = User.query.filter(User.email == email).first() #12:34
+            logged_user = User.query.filter(User.email == email).first()
+            if logged_user and check_password_hash(logged_user.password, password):
+                login_user(logged_user)
+                flash('You were successful in your initiation.', 'auth-success')
+                return redirect(url_for('site-profile'))
+            else:
+                flash('You have failed in your attempt to access this content.', 'auth-failed')
+    except:
+        raise Exception('Invalid form data: Please check your form')
+    return render_template('sign_in.html', form=form)
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('site.home'))
